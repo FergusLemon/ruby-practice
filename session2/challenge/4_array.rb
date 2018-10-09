53b36045dd621a3d9747e34eb62c06a3eeab8b82
Fergus Lemon <fergus.lemon@gmail.com>
2018-10-09 13:46:45 +0100

    Solutions to session2, challenges 4-5

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -7,3 +7,9 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+  def get_squares(array)
+    arr = array
+    new_arr = []
+    arr.select { |x| new_arr << x if arr.include?(x ** 2) }
+    new_arr.sort
+  end
--- session2/challenge/5_array.rb
+++ session2/challenge/5_array.rb
@@ -1,5 +1,4 @@
-# Write a function named mod_three which takes an array of numbers, 
-# and return a new array consisting of their remainder when divided by three.
+# Write a function named mod_three which takes an array of numbers, # and return a new array consisting of their remainder when divided by three.
 # Exclude any numbers which are actually dividible by three.
 # 
 # EXAMPLES:
@@ -13,3 +12,9 @@
 # mod_three [7]  # => [1]
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
+def mod_three(array)
+  arr = array
+  new_arr = []
+  arr.each.select { |x| new_arr << x%3 if x%3 != 0 }
+  new_arr
+end
