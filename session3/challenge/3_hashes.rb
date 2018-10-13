f567f2a05a2c1612be2b71a4515099de1ec561e0
Fergus Lemon <fergus.lemon@gmail.com>
2018-10-13 12:50:11 +0100

    Solutions to session3, challenge 3

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,13 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+def word_count(word)
+  arr = word.split()
+  to_inc = []
+  arr.map { |w| to_inc << w.downcase }
+  unique_words = to_inc.uniq
+  word_count = unique_words.map { |w| to_inc.count(w) }
+  to_return = {}
+  unique_words.map.with_index { |w, i| to_return[w] = word_count[i]}
+  to_return
+end
